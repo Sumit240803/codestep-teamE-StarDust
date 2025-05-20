f0936e74cae783888cc02010ab64219c113486dc
Sumit240803 <goyalsumit651@gmail.com>
2025-05-20 17:22:46 +0530

    UI improvments

--- src/game_frontend/src/pages/Dashboard/Exchange/index.tsx
+++ src/game_frontend/src/pages/Dashboard/Exchange/index.tsx
@@ -4,43 +4,51 @@ import '../index.css'
 import usePoints from "../../../hooks/usePoints"
 import Loader from "../../../components/ui/Loader"
 import { motion } from "framer-motion"
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 import Joyride from "react-joyride"
 import { useJoyRide } from "../../../context/JoyrideContext"
+import { exchangeSteps } from "../../../steps/appsteps"
 
 const Exchange = () => {
+  const particles = useMemo(() =>
+  Array.from({ length: 70 }).map(() => ({
+    top: `${Math.random() * 100}%`,
+    left: `${Math.random() * 100}%`,
+    duration: 5 + Math.random() * 5,
+  })), []
+);
+
   const { points, incrementPoints, isLoading } = usePoints(0)
   const [isPressed, setIsPressed] = useState(false);
-   const {setSteps , setRun} = useJoyRide();
-   useEffect(()=>{
-    setSteps([
-       {
-    target: ".step-1",
-    content: "Click on the astronaut to earn points! But that’s just one of many ways to boost your score.",
-  },
-  {
-    target: ".step-2",
-    content: "This progress bar visually fills up as you earn more points. Keep it growing!",
-  },
-  {
-    target: ".step-3",
-    content: "Explore more features and opportunities in StarDust from this sidebar menu.",
-  },
-    ])
-    setRun(true);
-   },[]);
+   const {setSteps , setRun,setStepIndex} = useJoyRide();
+   //const [show , setShow] = useState<string | null>("");
+   /*if(typeof window !== 'undefined'){
+      (console.log(localStorage.getItem('firstTime')))
+   }*/
+   useEffect(() => {
+  //  if(localStorage.getItem('firstTime') === "true"){
+
+      
+  
+  setSteps(exchangeSteps);
+  setStepIndex(0);
+  // Slight delay ensures steps are registered before starting
+  setTimeout(() => {setRun(true); localStorage.setItem("firstTime" , "false")}, 150); 
+//}
+}, []);
+
 
 
 
   return (
-    <main className="exchange-container  relative w-screen h-full">
+    <main className="exchange-container   relative w-screen h-full">
       {isLoading ? (
         <Loader />
       ) : (
         <>
           <section className="flex items-center justify-center w-screen font-coin z-10 ">
             <img
-              className="z-100 step-1"
+              className="z-100 "
               src="/assets/images/ufo.svg"
               alt="ufo"
               width={50}
@@ -66,29 +74,53 @@ const Exchange = () => {
           <Sidebar />
 
 
-          <motion.section
-            onClick={incrementPoints}
-            className={`click-to-earn ${isPressed ? "glow" : ""}`}
-            onMouseDown={() => setIsPressed(true)}
-            onMouseUp={() => setIsPressed(false)}
-            onMouseLeave={() => setIsPressed(false)}
-            whileTap={{ scale: 0.95 }}
-            transition={{ type: "spring", stiffness: 500 }}
-            style={{
-              backgroundImage: 'url("/assets/images/firefly-removebg-preview.png")',
-              position : 'fixed',
-              left : 650,
-              bottom: -190,
-              
-              width: '20%',
-              height: "100%",
-              
-             
-              backgroundRepeat: 'no-repeat',
-       
-              zIndex: 1,
-            }}
-          />
+         <motion.section
+  onClick={incrementPoints}
+  className={`step-1 click-to-earn ${isPressed ? "glow" : ""}`}
+  onMouseDown={() => setIsPressed(true)}
+  onMouseUp={() => setIsPressed(false)}
+  onMouseLeave={() => setIsPressed(false)}
+  whileTap={{ scale: 0.97 }}
+  transition={{ type: "spring", stiffness: 500 }}
+  style={{
+    backgroundImage: 'url("/assets/images/firefly-removebg-preview.png")',
+    position: 'fixed',
+    top: '30%',
+    left: '45%',
+   
+    width: '300px',     // adjust as needed
+    height: '500px',    // adjust as needed
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'contain',
+    zIndex: 1,
+  }}
+/>
+{particles.map((p, i) => (
+  <motion.div
+    key={i}
+    style={{
+      position: "absolute",
+      width: 6,
+      height: 6,
+      background: "rgb(121, 4, 195)",
+      borderRadius: "50%",
+      top: p.top,
+      left: p.left,
+    }}
+    animate={{
+      y: [-10, 10, -10],
+      x: [0, 5, 0],
+      opacity: [0.2, 1, 0.2],
+    }}
+    transition={{
+      repeat: Infinity,
+      duration: p.duration,
+      ease: "easeInOut",
+    }}
+  />
+))}
+
+
         </>
       )}
     </main>
---
+++ src/game_frontend/src/steps/appsteps.ts
@@ -0,0 +1,29 @@
+export const exchangeSteps = [
+     {
+      target: ".step-1",
+      content: "Click on the astronaut to earn points! But that’s just one of many ways to boost your score.",
+    },
+    {
+      target: ".step-2",
+      content: "This progress bar visually fills up as you earn more points. Keep it growing!",
+    },
+    {
+      target: ".step-3",
+      content: "Explore more features and opportunities in StarDust from this sidebar menu.",
+    },
+]
+
+export const airdropSteps = [
+   {
+      target: ".step-1",
+      content: "Click on the astronaut to earn points! But that’s just one of many ways to boost your score.",
+    },
+    {
+      target: ".step-2",
+      content: "This progress bar visually fills up as you earn more points. Keep it growing!",
+    },
+    {
+      target: ".step-3",
+      content: "Explore more features and opportunities in StarDust from this sidebar menu.",
+    },
+]
